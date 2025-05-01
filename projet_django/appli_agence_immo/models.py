b290b65a59b2cf38a67b1089c95f919bd20a1d22
Dorian <dorianllp2032@gmail.com>
2025-05-01 09:58:00 +0200

    models mis à jour et migrés

--- projet_django/appli_agence_immo/models.py
+++ projet_django/appli_agence_immo/models.py
@@ -3,13 +3,36 @@ from django.db import models
 # Create your models here.
 
 
-class Vendeurs(models.Model):
-
-
-
 class Acheteurs(models.Model):
+    id_acheteur=models.IntegerField(primary_key=True)
+    superficie=models.DecimalField(max_digits=6, decimal_places=2)
+    nbr_chambre=models.IntegerField()
+    garage=models.BooleanField()
+    ville=models.CharField(max_length=30)
 
+class Bien_Immobiliers(models.Model):
+    id_bien=models.IntegerField(primary_key=True)
+    ville=models.CharField(max_length=30)
+    code_postal=models.IntegerField()
+    surface=models.DecimalField(max_digits=6, decimal_places=2)
+    nb_chambres=models.IntegerField()
+    garage=models.BooleanField()
+    etat=models.CharField(max_length=10)
 
+class Vendeurs(models.Model):
+    id_vendeur=models.IntegerField(primary_key=True)
+    nom=models.CharField(max_length=30)
 
 
 class Agent_immo(models.Model):
+    id_agent=models.IntegerField(primary_key=True)
+    id_bien=models.IntegerField()
+
+class Achat(models.Model):
+    id_bien=models.ForeignKey(Bien_Immobiliers,on_delete=models.CASCADE)
+    id_vendeur=models.ForeignKey(Vendeurs,on_delete=models.CASCADE)
+    id_acheteur=models.ForeignKey(Acheteurs,on_delete=models.CASCADE)
+    id_agent=models.ForeignKey(Agent_immo,on_delete=models.CASCADE)
+    bien_achete=models.BooleanField()
+    bien_visite=models.BooleanField()
+    rdv=models.DateField()
